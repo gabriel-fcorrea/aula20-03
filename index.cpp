4e3c26d3b5459ac18d75959d1b325bb965edf0c8
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-03-20 20:28:32 -0300

    testando o dowhile

--- index.cpp
+++ index.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-using namespace std; 
 
-int main () {
+using namespace std;
+
+int main() {
+	int teste;
+	int base = 5;
+	cout << "digite um numero \n";
+	cin >> teste;
 	
+	do {
+		cout << "testando o dowhile" << teste << "vezes" << endl;
+		teste ++;
+	}
+	
+	while(teste < base);
 }
+
